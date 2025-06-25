0c08f3515a115128e4cb9a1b6cc287295e9a3dc0
koushikk12345678910 <147581431+koushikk12345678910@users.noreply.github.com>
2025-06-25 17:05:37 +0530

    day 2

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte a=12;
+		System.out.println("my age is\t\t:"+a);
+		short b=30000;
+		System.out.println("short number is\t\t:"+b);
+		int c=1234567898;
+		System.out.println("the long value is\t\t:"+c);
+		char m='r';
+		System.out.println("the character is\t:"+m);
+		float d=456.567f;
+		System.out.println("the decimal value is\t\t:"+d);
+		double e=34225.4678;
+		System.out.println("the long decimal value is\t:"+e);
+		boolean istrue=true;
+		System.out.println("the statement is\t\t:"+istrue);
+		System.out.println("add"+(8+8));
+		
+		
+		
+		
+	}
+
+}
