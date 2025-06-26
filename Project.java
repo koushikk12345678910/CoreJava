6da1120a785809b41a33da63843120f2d92cd6da
koushikk12345678910 <147581431+koushikk12345678910@users.noreply.github.com>
2025-06-26 14:32:47 +0530

    day2

---
+++ Project.java
@@ -0,0 +1,31 @@
+package day3;
+
+public class Project {
+	public static void main(String[] args) {
+		
+		String name1="koushik";
+		String adjective1="lazy";
+		String noun1="dogs";
+		String noun2="chicken";
+		String adjective2="stressful";
+		String verb1="dacing";
+		String noun3="surprise";
+		String noun4="cat";
+		String adjective3="surprise";
+		String place1="kodungaiyur";
+		String noun5="icecream";
+		String noun6="glad";
+		int num=2026;
+		
+		String story="this morning "+name1+" wokeup felling "+adjective1+" it is going to be "+adjective2
+				+" day!outside a bunch of "+noun1+" were prosting to keep "+noun2+" in storys.they began to "+verb1+" to the rythem of the "+noun3+
+				" which made all the "+noun4+" very "+adjective3+" concerned, "+name1+" texted "+noun5+" who flew "+noun1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+"."+name1+" woke up in the year "
+				+num+" , in the world where "+noun6+" s ruled the world";
+		
+		System.out.println(story);
+				
+		
+		
+	}
+
+}
